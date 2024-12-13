e817e61dad1d0f6684961f1861b56a71ebb30cab
khalvashi298 <khalvashim2@gmail.com>
2024-12-14 01:23:41 +0400

    LelaTask2.1

---
+++ src/main/java/org/example/Interface1.java
@@ -0,0 +1,7 @@
+
+package org.example;
+
+    public interface Interface1 {
+
+    void replaceSymbolA ();
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Symbol symbol = new Symbol();
+
+        symbol.replaceSymbolA();
+
+        symbol.replaceSymbolB();
+    }
+}
